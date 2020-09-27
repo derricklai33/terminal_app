114934a83e91bb340679b6dc253b6b9e6251365c
derricklai33 <derricklai33@hotmail.com>
2020-09-27 00:12:33 -0700

    Test to make sure output from HTTP request returns correct values

--- src/lib/menu.rb
+++ src/lib/menu.rb
@@ -1,17 +1,20 @@
 require_relative 'app_constants'
 
+# Menu Class
 class Menu
   include AppConstants
 
+  # Initialized instance variable as an instance of CallApi class
   def initialize
-    @restaurant = []
+    @restaurant_api = CallApi.new
   end
-  
+
+  # TTY prompt for user to select input
   def display_menu
-    PROMPT.select("Welcome!") do |menu|
-      menu.choice({ name: 'View all restaurants', value: '1'})
-      menu.choice({ name: 'Input new restaurants', value: '2'})
-      menu.choice({ name: "Exit", value: '3'})
+    PROMPT.select('Welcome!') do |menu|
+      menu.choice({ name: 'View all restaurants', value: '1' })
+      menu.choice({ name: 'Input new restaurants', value: '2' })
+      menu.choice({ name: 'Exit', value: '3' })
     end
   end
 
@@ -19,11 +22,16 @@ class Menu
     loop do
       case display_menu
       when '1'
-        puts "Hello"
+        @restaurant_api.restaurants.each do |value|
+          puts value.name
+          puts value.price
+          puts value.type_of_cuisine
+          puts value.location
+        end
       when '2'
-        puts "Hi"
+        puts 'Hi'
       when '3'
-        puts "bye"
+        exit
       end
     end
   end
