af70658dc687e1dc5a008742a1734f9fef766d1c
derricklai33 <derricklai33@hotmail.com>
2020-09-27 00:38:29 -0700

    Changed variable names for better understanding

--- src/lib/restaurant.rb
+++ src/lib/restaurant.rb
@@ -1,10 +1,14 @@
 class Restaurant
-  attr_reader :name, :price, :type_of_cuisine, :location
+  attr_reader :name, :price, :type_of_cuisine, :address
 
-  def initialize(name, price, type_of_cuisine, location)
+  def initialize(name, price, type_of_cuisine, address)
     @name = name
     @price = price
     @type_of_cuisine = type_of_cuisine
-    @location = location
+    @address = address
+  end
+
+  def to_a
+    [@name, @price, @type_of_cuisine, @address]
   end
 end
