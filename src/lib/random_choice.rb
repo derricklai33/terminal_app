7150d74655ed205202d9b9ff22d06bac11d0b392
derricklai33 <derricklai33@hotmail.com>
2020-09-27 19:08:17 -0700

    Added random_restaurant method to display randomized result from RandomChoice class

--- src/lib/random_choice.rb
+++ src/lib/random_choice.rb
@@ -1,7 +1,17 @@
+require_relative 'restaurantapi'
+require_relative 'restaurant'
+
 class RandomChoice
+  attr_reader :random
+
   def initialize
+    @random = generate_random
   end
 
   def generate_random
+    rand = CallApi.new
+    new_arr = rand.restaurants.map(&:to_a)
+
+    new_arr.sample
   end
 end
