852c1ac851bf924e8cbf99cf7a01e29b594aa09e
derricklai33 <derricklai33@hotmail.com>
2020-09-29 17:19:43 +1000

    Changed rating data type by explicitly casting to_f to respond to sorting method in sorting.rb

--- src/lib/restaurant.rb
+++ src/lib/restaurant.rb
@@ -1,16 +1,19 @@
+# Class to initialize restaurant object
 class Restaurant
   attr_reader :id, :name, :price, :type_of_cuisine, :address, :rating
 
-  # Class to initialize restaurant object
+  # Instance attributes for initialization. (More variables can be added given they are available from API)
   def initialize(id, name, price, type_of_cuisine, address, rating)
     @id = id
     @name = name
     @price = price
     @type_of_cuisine = type_of_cuisine
     @address = address
-    @rating = rating
+    # Changed rating data type to float to fit sorting method on sorting.rb
+    @rating = rating.to_f
   end
 
+  # Method to convert object into an array of array to fit terminal table format
   def to_a
     [@id, @name, @price, @type_of_cuisine, @address, @rating]
   end
