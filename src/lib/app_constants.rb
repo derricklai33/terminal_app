5d61861f77b415127abc238d12c020f9a796799e
derricklai33 <derricklai33@hotmail.com>
2020-09-29 11:59:41 +1000

    Added new instance variable @rating to display ratings of restaurant and updated HEADINGS constant to reflect on terminal table

--- src/lib/app_constants.rb
+++ src/lib/app_constants.rb
@@ -1,5 +1,5 @@
 # Constants used throughout application
 module AppConstants
   PROMPT = TTY::Prompt.new
-  HEADINGS = ['ID', 'Name', 'Price for 2', 'Type of cuisine', 'Address'].freeze
+  HEADINGS = ['ID', 'Name', 'Price for 2', 'Type of cuisine', 'Address', 'Rating'].freeze
 end
--- src/lib/restaurant.rb
+++ src/lib/restaurant.rb
@@ -1,16 +1,17 @@
 class Restaurant
-  attr_reader :id, :name, :price, :type_of_cuisine, :address
+  attr_reader :id, :name, :price, :type_of_cuisine, :address, :rating
 
   # Class to initialize restaurant object
-  def initialize(id, name, price, type_of_cuisine, address)
+  def initialize(id, name, price, type_of_cuisine, address, rating)
     @id = id
     @name = name
     @price = price
     @type_of_cuisine = type_of_cuisine
     @address = address
+    @rating = rating
   end
 
   def to_a
-    [@id, @name, @price, @type_of_cuisine, @address]
+    [@id, @name, @price, @type_of_cuisine, @address, @rating]
   end
 end
