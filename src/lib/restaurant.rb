1c4bc95d249b35717cbac4a7c87d06dbc24e7aed
derricklai33 <derricklai33@hotmail.com>
2020-10-01 14:37:56 +1000

    Added user input prompt under Restaurant class

--- src/lib/restaurant.rb
+++ src/lib/restaurant.rb
@@ -4,9 +4,9 @@ class Restaurant
 
   # Instance attributes for initialization. (More variables can be added given they are available from API)
   def initialize(id, name, price, type_of_cuisine, address, rating)
-    @id = id
+    @id = id.to_i
     @name = name
-    @price = price
+    @price = price.to_i
     @type_of_cuisine = type_of_cuisine
     @address = address
     # Changed rating data type to float to fit sorting method on sorting.rb
@@ -16,5 +16,16 @@ class Restaurant
   # Method to convert object into an array of array to fit terminal table format
   def to_a
     [@id, @name, @price, @type_of_cuisine, @address, @rating]
+  end
+
+  def self.restaurant_input
+    inputs = %w[name price cuisine location rating]
+    restaurants = {}
+    inputs.each do |prompt|
+      puts "Input the #{prompt}?"
+      print '> '
+      restaurants[prompt] = gets.chomp
+    end
+    restaurants
   end
 end
