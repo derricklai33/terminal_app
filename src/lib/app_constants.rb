dd2846796d0fe8d9b71b8535a17984db6b496fae
derricklai33 <derricklai33@hotmail.com>
2020-10-02 16:14:07 +1000

    Extracted inputs array from restaurant.rb to be a constant

--- src/lib/app_constants.rb
+++ src/lib/app_constants.rb
@@ -2,4 +2,5 @@
 module AppConstants
   PROMPT = TTY::Prompt.new
   HEADINGS = ['ID', 'Name', 'Price for 2', 'Type of cuisine', 'Address', 'Rating'].freeze
+  INPUTS = %w[name price cuisine location rating].freeze
 end
